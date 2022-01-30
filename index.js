9176133cf7959ce0edf95b6c9be2a4846aafe220
Michael Chan <mijoch@gmail.com>
2022-01-29 21:03:56 -0800

    add worker

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { answers, rest, all } from 'wordle-words'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +8,35 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  let url = new URL(request.url)
+  let endpoint = url.pathname.split('/')[1]
+  let dailyIndex = Math.floor(Date.now() / 8640000) - 189998
+
+  if (!endpoint) {
+    return new Response(JSON.stringify(answers[dailyIndex]), {
+      headers: { 'content-type': 'application/json' },
+    })
+  }
+
+  if (Number(endpoint) > 0 && Number(endpoint) < answers.length) {
+    return new Response(JSON.stringify(answers[Number(endpoint)]), {
+      headers: { 'content-type': 'application/json' },
+    })
+  }
+
+  if (endpoint === 'answers') {
+    return new Response(JSON.stringify(answers), {
+      headers: { 'content-type': 'application/json' },
+    })
+  }
+
+  if (endpoint === 'rest') {
+    return new Response(JSON.stringify(rest), {
+      headers: { 'content-type': 'application/json' },
+    })
+  }
+
+  return new Response(JSON.stringify(all), {
+    headers: { 'content-type': 'application/json' },
   })
 }
